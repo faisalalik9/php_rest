9d33e3b96fec8bdb04bfef59f573d3fbc9dfc762
Nova <66014401+faisalalik9@users.noreply.github.com>
2022-12-09 14:49:41 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $db_user     = 'root';
+    $db_password = '';
+    $db_name     = 'insaid';
+
+    $db = new PDO('mysql:host=127.0.0.1;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
+
+    $db->setAttribute(PDO::AFTER_EMULATE_PREPARES,false);
+    $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,true);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    define('APP_NAME', 'REQUEST_CALLBACK_FORM_DATA');
+
+?>
